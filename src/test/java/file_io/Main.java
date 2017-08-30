afde2f3733f00c2a2719938362f0e578662a0cb7
GannaChernyshova <anna.chernyshova91@gmail.com>
2017-08-30 09:30:22 +0300

    Update Main.java

--- src/test/java/file_io/Main.java
+++ src/test/java/file_io/Main.java
@@ -5,14 +5,16 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
+        //путь к файлу нужно в main передавать. иначе твоя программа заточена на один конкретный файл, находящийся в конкретном месте
+        //это не универсально
         ConvertFromFastaToCsv convertFromFastaToCsv = new ConvertFromFastaToCsv();
-        convertFromFastaToCsv.ConvertFile();
+        convertFromFastaToCsv.ConvertFile(); //метод с маленькой буквы
         System.out.println("Файл сконвертирован из fasta в csv");
 
         FindCountOfWord findCountOfWord = new FindCountOfWord();
         System.out.format("AGAAGGGA встречается %s раз\n", findCountOfWord.getCount());
 
-        DeleteFile deleteFile = new DeleteFile();
-        deleteFile.DeleteOutputFile();
+        DeleteFile deleteFile = new DeleteFile(); //зачем целый класс для удаления файла. 
+        deleteFile.DeleteOutputFile(); //метод с маленькой буквы
     }
 }
